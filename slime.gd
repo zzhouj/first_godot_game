4f344235a3cdf334bcf9685f9484fc7945f23f25
Justin <457299@qq.com>
2025-05-31 11:45:46 +0800

    slime ping pong

---
+++ slime.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+const SPEED = 60.0
+
+var direction = 1
+
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if ray_cast_right.is_colliding():
+		direction = -1
+		animated_sprite_2d.flip_h = true
+	if ray_cast_left.is_colliding():
+		direction = 1
+		animated_sprite_2d.flip_h = false
+
+	position.x += direction * SPEED * delta
