5c58a758cb0e3a451e8fd7a71402b6575fe00b1d
Justin <457299@qq.com>
2025-05-31 11:47:25 +0800

    dying upgrade

--- killzone.gd
+++ killzone.gd
@@ -5,8 +5,11 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You died!")
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
